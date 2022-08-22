a04ebf6111290a6d4a37fc7e13c39adca8402053
xiaozx <xiaozx@shinetechchina.com>
2022-08-23 01:22:39 +0800

    - init project

---
+++ app/index.js
@@ -0,0 +1,28 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const isDev = require('electron-is-dev');
+
+let mainWindow;
+
+let createWindow = () => {
+    mainWindow = new BrowserWindow({
+        width: 900,
+        height: 700,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            preload: path.join(__dirname, 'preload.js'),
+        }
+    });
+
+    mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${__dirname}/../build/index.html`);
+    isDev && mainWindow.webContents.openDevTools();
+
+    mainWindow.on('closed', () => mainWindow = null);
+}
+
+app.on('ready', () => createWindow());
+
+app.on('activate', () => {if(mainWindow === null) createWindow();});
+
+app.on('window-all-closed', () => {if(process.platform !== 'darwin') app.quit();});
